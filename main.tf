868f842a98d5ac9386ddb713b5ee2ecffd1e9a45
aman <aman@asterdio.com>
2022-10-14 11:22:10 +0545

    additional comments

---
+++ main.tf
@@ -0,0 +1,33 @@
+# Terraform provider to be used
+
+terraform {
+  required_providers {
+
+
+    }
+  }
+
+
+# Authentication method for the provider to be used in terraform
+  provider "" {
+    email = var.auth_email
+    api_key =  var.auth_key
+  }
+
+# Authentication variable i.e email and auth key
+
+# These values are taken from secret.tfvars for security purposes while tracking changes and pushing it to github
+
+  variable "auth_email" {
+    description = "Email authentication for auth"
+    type        = string
+    sensitive   = true
+  }
+
+  variable "auth_key" {
+    description = "Key for auth"
+    type        = string
+    sensitive   = true
+  }  
+
+# You can specify the modules here
---
+++ secret.tfvars
@@ -0,0 +1,4 @@
+# Authenctication tokens are provided here. The variable names should be matched as in main.tf
+
+auth_email=""
+auth_key=""
